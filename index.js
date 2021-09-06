3f0d28d6c49b27101681508c61f3d7d5db5333ef
ELATREDNU-SNAS <70924774+ELATREDNU-SNAS@users.noreply.github.com>
2021-09-06 16:30:41 +1000

    0.0.4

--- index.js
+++ index.js
@@ -1,58 +1,98 @@
 import { request } from "../requestV2";
-i = 0
-it_no = 0
-auctions = null
+i = 0;
+it_no = 0;
+auctions = null;
 
-register("tick", ticker)
+register("tick", ticker);
 function ticker() {
-	i++
-	if (i > 600) {
-		i = 0
-request({
-    url: 'https://auction-destroyer.herokuapp.com/',
-    json: true,
-		connectTimeout: 1000
-})
-    .then(function(response) {
-        // If the website does not return valid JSON data,
-        // then the parsing will fail and the catch block
-        // will run
-        auctions = response
+  i++;
+  if (i > 600 && found === 0) {
+    i = 0;
+    request({
+      url: "https://auction-destroyer.herokuapp.com/",
+      json: true,
+      connectTimeout: 1000,
+    }).then(function (response) {
+      auctions = response;
+      found = 1
+      try {
+        auc = auctions[it_no];
+        try {
+          ChatLib.clearChat(5050);
+          ChatLib.clearChat(5051);
+          ChatLib.clearChat(5052);
+        } catch (e) {}
+        new Message(
+          new TextComponent(
+            `&l&f${auc.name} &r| &6Avg: $${numberWithCommas(
+              auc.average_price
+            )}&r| &6Price: $${numberWithCommas(auc.price)}`
+          )
+        )
+          .setChatLineId(5050)
+          .chat();
+        new Message(
+          new TextComponent("&l&2[To Auction] ").setClick(
+            "run_command",
+            `/viewauction ${auc.seller}`
+          )
+        )
+          .setChatLineId(5051)
+          .chat();
+        new Message(new TextComponent("&l&4[Next]").setClick("run_command", `/af`))
+          .setChatLineId(5052)
+          .chat();
 
-				//ChatLib.chat('New auctions are in!')
-				//it_no = 0
+        it_no = Math.floor(Math.random() * auctions.length);
 
+      } catch (e) {
+        //ChatLib.chat(response.toString());
+      }
     });
+  }
 }
 
+register("command", changer).setName("af");
+
+
+function changer() {
+  found = 0
 }
-
-register('command', auctionRoute)
-.setName('af')
-
 function auctionRoute() {
+  try {
+    auc = auctions[it_no];
+    try {
+      ChatLib.clearChat(5050);
+      ChatLib.clearChat(5051);
+      ChatLib.clearChat(5052);
+    } catch (e) {}
+    new Message(
+      new TextComponent(
+        `&l&f${auc.name} &r| &6Avg: $${numberWithCommas(
+          auc.average_price
+        )}&r| &6Price: $${numberWithCommas(auc.price)}`
+      )
+    )
+      .setChatLineId(5050)
+      .chat();
+    new Message(
+      new TextComponent("&l&2[To Auction] ").setClick(
+        "run_command",
+        `/viewauction ${auc.seller}`
+      )
+    )
+      .setChatLineId(5051)
+      .chat();
+    new Message(new TextComponent("&l&4[Next]").setClick("run_command", `/af`))
+      .setChatLineId(5052)
+      .chat();
 
-try {
-	auc = auctions[it_no]
-try {
-ChatLib.clearChat(5050);
-ChatLib.clearChat(5051);
-ChatLib.clearChat(5052);
-} catch(e) {
-
+    it_no = Math.floor(Math.random() * auctions.length);
+  } catch (e) {
+    ChatLib.chat('No Auction Flips Found');
+  }
 }
-new Message(new TextComponent(`&l&f${auc.name} &r| &6Avg: $${numberWithCommas(auc.average_price)}&r| &6Price: $${numberWithCommas(auc.price)}`)).setChatLineId(5050).chat();
-new Message(new TextComponent("&l&2[To Auction] ").setClick("run_command", `/viewauction ${auc.seller}`)).setChatLineId(5051).chat();
-new Message(new TextComponent("&l&4[Next]").setClick("run_command", `/af`)).setChatLineId(5052).chat();
-
-it_no = Math.floor(Math.random() * auctions.length);
-}catch(e){
-	ChatLib.chat('No auctions in cache.')
-}
-}
-
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
