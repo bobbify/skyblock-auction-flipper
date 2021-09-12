cbf772cb6254c7cc14cd8f3b8cc236d3d63449d5
ELATREDNU-SNAS <70924774+ELATREDNU-SNAS@users.noreply.github.com>
2021-09-13 01:35:25 +1000

    0.0.6

--- index.js
+++ index.js
@@ -24,25 +24,18 @@ Setting.register(setting);
 i = 0;
 it_no = 0;
 auctions = null;
-  lines = []
+
 found = 0
-  function arrayContains(needle, arrhaystack)
-  {
-      return (arrhaystack.indexOf(needle) > -1);
-  }
+
 
 register("tick", ticker);
 function ticker() {
-lines = []
-  Scoreboard.getLines().forEach((item, i) => {
-    lines.push(ChatLib.removeFormatting(item).replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ''))
-  });
+
 
 
   i++;
-  if (i > 600 && found === 0 && arrayContains("  BarackObama1961's Island",lines) && setting.getSetting("Main", "Enabled") === true) {
+  if (i > 600 && found === 0 && setting.getSetting("Main", "Enabled") === true) {
     i = 0;
-
     request({
       url: "https://auction-destroyer.herokuapp.com/",
       json: true,
@@ -94,12 +87,7 @@ register("command", changer).setName("af");
 
 
 function changer() {
-  if (arrayContains("  BarackObama1961's Island",lines)) {
   found = 0
-}else{
-  ChatLib.chat('Limiting usage due to testing of new features, /visit BarackObama1961 in order to use the flipper.')
-}
-
 }
 function auctionRoute() {
 
